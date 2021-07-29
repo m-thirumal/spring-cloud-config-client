7a3480b256afe09caf858e615a76bf605ad08713
Thirumal <m.thirumal@hotmail.com>
2021-07-29 18:34:45 +0530

    Initial commit

---
+++ src/main/java/in/thirumal/SpringCloudConfigClientApplication.java
@@ -0,0 +1,17 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Thirumal
+ *
+ */
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+
+}
